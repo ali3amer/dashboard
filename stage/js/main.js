bf04237b4e9cf015e93b805c1f41ef24b01ba0eb
ali3amer <aliaamer549@gmail.com>
2020-01-02 00:25:27 +0200

    Lesson 12 - 13 - 14

--- stage/js/main.js
+++ stage/js/main.js
@@ -3,4 +3,12 @@ $(function () {
     $('.toggle-sidebar').on('click', function () {
         $(".content-area, .sidebar").toggleClass("no-sidebar");
     });
+    $(".toggle-submenu").on("click", function() {
+      $(this)
+        .find(".fa-angle-right")
+        .toggleClass("down");
+      $(this)
+        .next(".child-links")
+        .slideToggle();
+    });
 });
