9836bdcbf4e3e1271ce1f8c11aa98be9b2bbc7e4
ali3amer <aliaamer549@gmail.com>
2020-01-03 19:58:21 +0200

    Lesson 27

--- stage/js/main.js
+++ stage/js/main.js
@@ -41,6 +41,23 @@ $(function () {
         .addClass($(this).data("theme"));
     });
 
+    var fontClasses = [];
+    $(".font-options select option").each(function() {
+      fontClasses.push($(this).val());
+    });
+
+    $(".font-options select").on("change", function() {
+      $("body")
+        .removeClass(fontClasses.join(" "))
+        .addClass(
+          $(this)
+            .find("option:selected")
+            .val()
+        );
+    });
+
+    
+
     /* Get the documentElement (<html>) to display the page in fullscreen */
     var elem = document.documentElement;
 
