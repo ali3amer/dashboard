161148c93ef7e365c72f848d7ff1234dac12d6b4
ali3amer <aliaamer549@gmail.com>
2020-01-03 11:59:50 +0200

    Lesson 21

--- stage/js/main.js
+++ stage/js/main.js
@@ -21,6 +21,13 @@ $(function () {
       }
     });
 
+    $(".toggle-settings").on("click", function() {
+      $(this).find("i").toggleClass("fa-spin");
+      $(this)
+        .parent()
+        .toggleClass("hide-settings");
+    });
+
     /* Get the documentElement (<html>) to display the page in fullscreen */
     var elem = document.documentElement;
 
